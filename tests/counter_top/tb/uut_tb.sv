6b22c0ed4167cf3a5153d804ab2431f5d18b6d86
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-03-07 16:23:02 -0300

    updated counter_top run

--- tests/counter_top/tb/uut_tb.sv
+++ tests/counter_top/tb/uut_tb.sv
@@ -1,4 +1,26 @@
 module uut_tb(
     uut_if.master _if
 );
+    // initial values
+    initial begin
+        _if.clk = $random();
+    end
+
+    // Clock gen
+    always begin
+        #5 _if.clk = ~_if.clk;
+    end
+
+    // reset assign
+    initial begin
+        _if.a_rst = 1;
+        #17 _if.a_rst = 0;
+    end
+
+    // finish sim
+    initial begin
+        repeat (20) @(posedge _if.clk);
+        $finish();
+    end
+
 endmodule

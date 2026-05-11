5425b6664e1a13acfdd4311233e00ea4adbe3a9b
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-05-11 16:06:35 -0300

    arg to infer domains + arg to control dependency print

--- tests/common/static.mk
+++ tests/common/static.mk
@@ -10,13 +10,15 @@ STATIC_BUILD_DIR = $(PROJECT_ROOT)/build/sanitized
 RTL_PKGS = $(shell find -L $(RTL_DIR) -type f -name "*_pkg.sv")
 RTL_SRCS = $(RTL_PKGS) $(shell find -L $(RTL_DIR) -type f \( -name "*.v" -o -name "*.sv" \) | grep -v "_pkg.sv")
 DOMAINS_YAML = $(RTL_DIR)/$(MODULE_NAME).domains.yaml
-DOMAINS_ARG = $(if $(wildcard $(DOMAINS_YAML)),--domains $(DOMAINS_YAML),)
+INFER_TOP_DOMAINS_ARG = $(if $(INFER_TOP_DOMAINS),--infer-top-domains,)
+DOMAINS_ARG = $(if $(INFER_TOP_DOMAINS),,$(if $(wildcard $(DOMAINS_YAML)),--domains $(DOMAINS_YAML),))
 
 STATIC_ANALYZER = $(STATIC_BUILD_DIR)/mate
 SANITIZER_ENV = ASAN_OPTIONS=symbolize=1,detect_leaks=0,abort_on_error=1 UBSAN_OPTIONS=print_stacktrace=1,halt_on_error=1
 
 # Per-test optional args (from static.args file)
 EXTRA_ARGS = $(shell cat static.args 2>/dev/null)
+DEBUG_NODE_DEPS_ARG = $(if $(DEBUG_NODE_DEPS),--debug-node-deps $(DEBUG_NODE_DEPS),)
 
 all: analyze
 
@@ -24,7 +26,9 @@ analyze:
 	$(MAKE) -C $(PROJECT_ROOT) $(STATIC_BUILD_TARGET)
 	rm -rf debug_output
 	$(SANITIZER_ENV) $(STATIC_ANALYZER) --analyze --top $(MODULE_NAME) \
+		$(INFER_TOP_DOMAINS_ARG) \
 		$(DOMAINS_ARG) \
+		$(DEBUG_NODE_DEPS_ARG) \
 		$(EXTRA_ARGS) $(RTL_SRCS)
 
 clean:

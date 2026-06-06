8fc143dcd656269f71fea63ccb5bb57fbf01adb2
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-06-06 14:47:58 -0300

    recorder with unpacked array ports

--- tests/common/tb/recorder.sv
+++ tests/common/tb/recorder.sv
@@ -1,19 +1,20 @@
 module recorder#(
     string filepath,
     type TYPE,
-    bit IS_SYNC
+    bit IS_SYNC,
+    int LENGTH
 )(
     input logic clk,
-    input TYPE data
+    input TYPE data [LENGTH]
 );
     integer file;
     TYPE _last;
 
     function automatic void write();
         if (IS_SYNC)
-            $fwrite(file, "0x%h (%d)\n", data, data);
+            $fwrite(file, "0x%h (%d)\n", data[0], data[0]);
         else
-            $fwrite(file, "%0t 0x%h (%d)\n", $realtime, data, data);
+            $fwrite(file, "%0t 0x%h (%d)\n", $realtime, data[0], data[0]);
     endfunction
 
     initial begin
@@ -26,7 +27,7 @@ module recorder#(
             $fwrite(file, "# value at each clock\n");
         else begin
             $fwrite(file, "# time value followed by value\n");
-            #0 _last = data;
+            #0 _last = data[0];
             write();
         end
     end
@@ -34,19 +35,19 @@ module recorder#(
     if (IS_SYNC) begin : g_sync
         always @(posedge clk) begin
             write();
-            _last <= data;
+            _last <= data[0];
         end
     end else begin : g_async
         always @(data) begin
-            if (data !== _last) begin
+            if (data[0] !== _last) begin
                 write();
-                _last = data;
+                _last = data[0];
             end
         end
     end
 
     final begin
-        if (IS_SYNC && _last !== data)
+        if (IS_SYNC && _last !== data[0])
             write();
         $fclose(file);
     end

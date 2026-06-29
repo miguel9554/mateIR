75e13b5fa99ddffd05ea54fff554e4ccb6ceb502
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2026-06-29 00:20:48 -0300

    fixed undriven bits in test

--- tests/submodule_output_bitselect_partial/rtl/submodule_output_bitselect_partial.v
+++ tests/submodule_output_bitselect_partial/rtl/submodule_output_bitselect_partial.v
@@ -4,6 +4,9 @@ module submodule_output_bitselect_partial (
     output wire [3:0] out_bus
 );
     wire [3:0] bus;
+    assign bus[0] = 1;
+    assign bus[1] = in_bus[1];
+    assign bus[3] = 0;
     child_out u0(.d(in_bus[0]), .q(bus[2]));
     assign out_bus = bus;
 endmodule
